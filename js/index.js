8a5ea5af2de47f2aad2c09df4c180b16e9a89feb
Khandoker Tahmid Sami <54868101+khandoker-tahmid-sami@users.noreply.github.com>
2022-10-29 15:40:20 +0200

    first

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// javascript for contact us form
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "prinon96@gmail.com",
+//         Password : "jamesbond0727",
+//         To : 'mkitchenweb@gmail.com',
+//         From : document.getElementById("email").value,
+//         Subject : "Contact Enquiry",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     ); 
+// }
